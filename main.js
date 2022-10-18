a8913f1b5844602d6966096988b17590192ef5ca
LeonardoSaraceno <leonardosaraceno03@gmail.com>
2022-10-18 17:50:59 +0200

    finito(?)

---
+++ main.js
@@ -0,0 +1,7 @@
+let nome = prompt('Scrivi il tuo nome')
+let cognome = prompt('Scrivi il tuo cognome')
+let colore = prompt('Scrivi il tuo colore preferito')
+
+
+
+document.getElementById('frasejavascript').innerHTML = `la tua password brutta è ${nome}${cognome}${colore}2`
